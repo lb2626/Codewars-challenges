e2e701a385ab743204e07f1976554e630d09caa9
lb2626 <lb93382@gmail.com>
2020-03-05 20:55:05 +0700

    added description to sum of nums

--- sum-of-numbers.py
+++ sum-of-numbers.py
@@ -1,4 +1,28 @@
 
+"""
+Codewars doesn't really give a great explanation of what they would like you to do. So here is one...
+
+Given two integers a and b, which can be positive or negative, find the sum of all the numbers in between (including the start and end) 
+and return it. If the two numbers are equal return a or b. If a and b are equal, return a or b.
+
+Note: a and b are not ordered!
+
+So if both are equal, I'm just going to return a.
+
+Next, we need to start from the smaller number to be able to loop through the integers and add them together.
+I'm using the sum method to add the numbers within the range of +1. And that's it!
+You could also do a simpler looking for loop and add the sums to a variable which starts at 0.
+
+For example
+n = 0
+
+elif a > b:
+    for i in range(b, a +1):
+        n += i
+"""
+
+
+
 
 def get_sum(a,b):
     if a == b:                               
@@ -8,3 +32,5 @@ def get_sum(a,b):
     elif a > b:
         return sum(range(b,a +1))
   
+
+
